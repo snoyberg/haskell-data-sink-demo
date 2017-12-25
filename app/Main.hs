d6ab8eed92bb623e4a2b78f8fbe9169d92e66abc
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2017-12-25 15:18:08 -0500

    app/Main.hs: update wai app to route 3 different paths
    Routes are like:
    
    /       -> POST message
    /status -> returns JSON {"status": "online"} message
    _       -> print "hello world" (_ signifies "any" here)
    
    This also demonstrates how to encode a super basic JSON dictionary in an
    HTTP response with wai.

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,46 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 import           Control.Exception        (SomeException)
 import           Control.Exception.Lifted (handle)
 import           Control.Monad.IO.Class   (liftIO)
-import           Data.Aeson               (Value, encode, object, (.=))
+import           Data.Aeson               (Value, encode, object, (.=), FromJSON, ToJSON)
 import           Data.Aeson.Parser        (json)
 import           Data.ByteString          (ByteString)
 import           Data.Conduit             (($$))
 import           Data.Conduit.Attoparsec  (sinkParser)
+import           Data.Text
+import           GHC.Generics
 import           Network.HTTP.Types       (status200, status400)
-import           Network.Wai              (Application, Response, responseLBS)
+import           Network.Wai              (Application, Response, responseLBS, pathInfo)
 import           Network.Wai.Conduit      (sourceRequestBody)
 import           Network.Wai.Handler.Warp (run)
+
+data StatusMsg = StatusMsg { status :: Text } deriving (Generic, Show)
+
+instance FromJSON StatusMsg
+instance ToJSON   StatusMsg
 
 main :: IO ()
 main = run 2378 app
 
 app :: Application
 app req sendResponse = handle (sendResponse . invalidJson) $ do
-    value <- sourceRequestBody req $$ sinkParser json
-    newValue <- liftIO $ modValue value
-    sendResponse $ responseLBS
-        status200
-        [("Content-Type", "application/json")]
-        $ encode newValue
+    case pathInfo req of
+      [] -> do
+        value <- sourceRequestBody req $$ sinkParser json
+        newValue <- liftIO $ modValue value
+        sendResponse $ responseLBS
+            status200
+            [("Content-Type", "application/json")]
+            $ encode newValue
+      ["status"] -> do
+        sendResponse $ responseLBS
+            status200 --[] "status: online"
+            [("Content-Type", "application/json")]
+            $ encode (StatusMsg { status = "online" })
+      _ -> do
+        sendResponse $ responseLBS
+            status200 [] "hello world!"
 
 invalidJson :: SomeException -> Response
 invalidJson ex = responseLBS
